827c81358d59b44eba68e68ee24bee1e19408bca
inuscript <inuscript@gmail.com>
2015-08-21 09:44:32 +0900

    ignore

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ PATH="./node_modules/.bin":$PATH
 browserify -e ./src/entry1.js | js-beautify -f - > build/default.js
 browserify -e ./src/entry1.js -u ./lib/*.js | js-beautify -f - > build/exclude.js
 browserify -e ./src/entry1.js -x ./lib/*.js | js-beautify -f - > build/external.js
+browserify -e ./src/entry1.js -i ./lib/*.js | js-beautify -f - > build/ignore.js
 browserify -r ./lib/*.js | js-beautify -f - > build/require.js
 browserify -s ./lib/some-lib.js | js-beautify -f - >  build/standalone.js
